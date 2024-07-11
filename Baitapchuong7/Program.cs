6c797efb3e99dd08cd46b769b41108228b30427d
dangtiendat24 <148854633+dangtiendat24@users.noreply.github.com>
2024-07-11 11:00:21 +0700

    Add files via upload

---
+++ Baitapchuong7/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Collections.Generic;
+
+namespace Bai47
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Dictionary<int, string> dictionary = new Dictionary<int, string>();
+
+            dictionary.Add(1, "Apple");
+            dictionary.Add(2, "Banana");
+            dictionary.Add(3, "Chery");
+            dictionary.Add(4, "Watermelon");
+
+            int keyToCheck = 1;
+            if(dictionary.ContainsKey(keyToCheck))
+            {
+                Console.WriteLine($"key {keyToCheck} tồn tại trong từ điển");
+            }
+            else
+            {
+                Console.WriteLine($"key {keyToCheck} không tồn tại");
+            }
+
+            int keyToRetrieve = 2;
+            if(dictionary.TryGetValue(keyToRetrieve, out string value))
+            {
+                Console.WriteLine($"Giá trị với key {keyToRetrieve} là: {value}");
+            }
+            else
+            {
+                Console.WriteLine($"key {keyToRetrieve} không tìm thấy trong từ điển");
+            }
+            Console.WriteLine("DANH SÁCH");
+            foreach(KeyValuePair<int, string> kvp in  dictionary)
+            {
+                Console.WriteLine($"Key: {kvp.Key}, value: {kvp.Value}");
+            }
+        }
+    }
+}
